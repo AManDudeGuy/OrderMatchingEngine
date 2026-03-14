39a42bb9caa1aa69fd061146de6f0d32b18f9219
Ali <syedaliabbask37@gmail.com>
2026-03-14 12:54:25 +0400

    Added naive order parsing

--- source/main.cpp
+++ source/main.cpp
@@ -2,24 +2,26 @@
 #include <string>
 #include <fstream>
 
-// Order Matching Engine
+const int BUY = 0;
+const int SELL = 1;
 
 struct OrderMessage{
-    std::string ticker{};
-    std::string buyOrSell{};
-    double price{};
+    std::string_view ticker{};
+    int buyOrSell{};
+    int price{}; // in cents to avoid floating point errors
     int amountOfShares{};
 };
 
-int main(){
+int main(int argc, char* argv[]){
 
-    std::fstream file("example.txt", std::ios::out);
-
-    for(int i = 0; i < 10000; i++){
-        file << i << "\n";
-    }
-
-    file.close();
 
     return 0;
 }
+help
+// TODO:
+// Implement storage of buy sell orders
+// Implement order execution logic
+// Implement recieving of bids and asks and acknowledgements
+// Implement order cancelation
+// Implement per stock
+// Make it FAST
